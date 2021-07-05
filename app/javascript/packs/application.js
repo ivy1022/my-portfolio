16c4f980c2e37d8b284a9337c35822ab1e0c87de
Ayaka Asano <yoonhak.x.aa@gmail.com>
2021-07-05 20:46:32 +0900

    added about & project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,4 @@ import '../stylesheets/application';
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+
